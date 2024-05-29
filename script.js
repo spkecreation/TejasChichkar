c1a003509786fb0ad256d29d30c56d38d8618caf
Rishabh Raj <digitalartbyraj@gmail.com>
2024-05-29 18:56:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.container div');
+    let currentIndex = 0;
+
+    document.querySelector('.arrow-left').addEventListener('click', () => {
+        images[currentIndex].classList.remove('active');
+        currentIndex = (currentIndex > 0) ? currentIndex - 1 : images.length - 1;
+        images[currentIndex].classList.add('active');
+    });
+
+    document.querySelector('.arrow-right').addEventListener('click', () => {
+        images[currentIndex].classList.remove('active');
+        currentIndex = (currentIndex < images.length - 1) ? currentIndex + 1 : 0;
+        images[currentIndex].classList.add('active');
+    });
+
+    document.querySelector('.contact').addEventListener('click', () => {
+        window.location.href = "https://www.google.com";
+    });
+    document.querySelector('.container').addEventListener('click', () => {
+        window.location.href = "https://www.google.com";
+    });
+});
